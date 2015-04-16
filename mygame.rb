16ba4d13d6b4ea3b8ef912381d00260e10303288
Ronald Joo <rjoo189@gmail.com>
2015-04-17 07:30:20 +0900

    starts game and machine classes

--- mygame.rb
+++ mygame.rb
@@ -13,30 +13,74 @@ def start_game
 	game = JSON.parse(game_json)
 end
 
+	HOST = 'http://job-queue-dev.elasticbeanstalk.com'
 class Game
 
-	HOST = 'http://job-queue-dev.elasticbeanstalk.com'
 
 	def initialize
 		game_json = RestClient.post("#{HOST}/games", {}).body
 		@game = JSON.parse(game_json)
-		@game_id = @game['id']
-		@status = ''
+		@machines = []
 	end
 
 	def next_turn
 		turn_json = RestClient.get("#{HOST}/games/#{@game['id']}/next_turn").body
-		turn = JSON.parse(turn_json)
-		@status = turn['status']
-		@jobs = turn['jobs']
+		@current_turn = JSON.parse(turn_json)
+		@status = @current_turn['status']
+		@jobs = @current_turn['jobs']
 	end
+
+
+	# sorts jobs according to their memory required, descending
+	def sort_jobs
+		@jobs.sort{|x,y| y['memory_required'] <=> x['memory_required']}
+	end
+
+	def play_turn
+		until @game['completed']
+			next_turn
+			if @jobs.any?
+				job_list = sort_jobs
+
+				job_list.each do |job|
+					@machines.each do |machine|
+						if machine.remaining_memory >= job['memory_required']
+							machine.assign(job['id'])
+						end
+				end
+
+			end
+
+		end
+
+	end
+
 
 end
 
 class Machine
 
+	def initialize(game_id)
+		@game_id = game_id
+		@machine_json = RestClient.post("#{HOST}/games/#{game_id}/machines").body
+		@machine = JSON.parse(machine_json)
+		@remaining_memory = 64
+	end
+
+	def terminate
+		RestClient.delete("#{HOST}/games/#{@game_id}/machines/#{@machine['id']}")
+	end
+
+	def assign(job_id)
+		RestClient.post("#{HOST}/games/#{@game_id}/machines/#{@machine['id']}/job_assignments",
+			job_ids: JSON.dump(job_id))
+	end
+
 end
 
 a = Game.new
+a.next_turn
+p a
+puts " ----- "
 a.next_turn
 p a
